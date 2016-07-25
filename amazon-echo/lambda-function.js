ad024c1b95b57b6007edeb71a36ea69c147f7639
Jordan Leigh <AlwaysBCoding@gmail.com>
2016-07-25 03:34:17 -0700

    adds amazon echo source code

---
+++ amazon-echo/lambda-function.js
@@ -0,0 +1,126 @@
+var https = require('https')
+
+exports.handler = (event, context) => {
+
+  try {
+
+    if (event.session.new) {
+      // New Session
+      console.log("NEW SESSION")
+    }
+
+    switch (event.request.type) {
+
+      case "LaunchRequest":
+        // Launch Request
+        console.log(`LAUNCH REQUEST`)
+        context.succeed(
+          generateResponse(
+            buildSpeechletResponse("Welcome to an Alexa Skill, this is running on a deployed lambda function", true),
+            {}
+          )
+        )
+        break;
+
+      case "IntentRequest":
+        // Intent Request
+        console.log(`INTENT REQUEST`)
+
+        switch(event.request.intent.name) {
+          case "GetSubscriberCount":
+            var endpoint = "" // ENDPOINT GOES HERE
+            var body = ""
+            https.get(endpoint, (response) => {
+              response.on('data', (chunk) => { body += chunk })
+              response.on('end', () => {
+                var data = JSON.parse(body)
+                var subscriberCount = data.items[0].statistics.subscriberCount
+                context.succeed(
+                  generateResponse(
+                    buildSpeechletResponse(`Current subscriber count is ${subscriberCount}`, true),
+                    {}
+                  )
+                )
+              })
+            })
+            break;
+
+          case "GetVideoViewCount":
+            var endpoint = "" // ENDPOINT GOES HERE
+            var body = ""
+            https.get(endpoint, (response) => {
+              response.on('data', (chunk) => { body += chunk })
+              response.on('end', () => {
+                var data = JSON.parse(body)
+                var viewCount = data.items[0].statistics.viewCount
+                context.succeed(
+                  generateResponse(
+                    buildSpeechletResponse(`Current view count is ${viewCount}`, true),
+                    {}
+                  )
+                )
+              })
+            })
+            break;
+
+          case "GetVideoViewCountSinceDate":
+            console.log(event.request.intent.slots.SinceDate.value)
+            var endpoint = "" // ENDPOINT GOES HERE
+            var body = ""
+            https.get(endpoint, (response) => {
+              response.on('data', (chunk) => { body += chunk })
+              response.on('end', () => {
+                var data = JSON.parse(body)
+                var viewCount = data.items[0].statistics.viewCount
+                context.succeed(
+                  generateResponse(
+                    buildSpeechletResponse(`Current view count is ${viewCount}`, true),
+                    {}
+                  )
+                )
+              })
+            })
+            break;
+
+          default:
+            throw "Invalid intent"
+        }
+
+        break;
+
+      case "SessionEndedRequest":
+        // Session Ended Request
+        console.log(`SESSION ENDED REQUEST`)
+        break;
+
+      default:
+        context.fail(`INVALID REQUEST TYPE: ${event.request.type}`)
+
+    }
+
+  } catch(error) { context.fail(`Exception: ${error}`) }
+
+}
+
+// Helpers
+buildSpeechletResponse = (outputText, shouldEndSession) => {
+
+  return {
+    outputSpeech: {
+      type: "PlainText",
+      text: outputText
+    },
+    shouldEndSession: shouldEndSession
+  }
+
+}
+
+generateResponse = (speechletResponse, sessionAttributes) => {
+
+  return {
+    version: "1.0",
+    sessionAttributes: sessionAttributes,
+    response: speechletResponse
+  }
+
+}
