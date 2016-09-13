e0513304ddf437546512fc2e140716ea6fc6690c
Jordan Leigh <AlwaysBCoding@gmail.com>
2016-09-13 14:20:18 -0700

    adds simple react

---
+++ simplereact/src/App.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      PLAYER_ONE_SYMBOL: "X",
+      PLAYER_TWO_SYMBOL: "O",
+      currentTurn: "X",
+      board: [
+        "", "", "", "", "", "", "", "", ""
+      ],
+      winner: null,
+    }
+  }
+
+  handleClick(index) {
+    if(this.state.board[index] === "") {
+      this.state.board[index] = this.state.currentTurn
+      const winner = this.checkForWinner()
+      this.setState({
+        board: this.state.board,
+        currentTurn: this.state.currentTurn === this.state.PLAYER_ONE_SYMBOL ? this.state.PLAYER_TWO_SYMBOL : this.state.PLAYER_ONE_SYMBOL,
+        winner: winner,
+      })
+    }
+  }
+
+  checkForWinner() {
+    var currentTurn = this.state.currentTurn
+    var symbols = this.state.board
+    var winningCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    return winningCombos.find(function(combo) {
+      if(symbols[combo[0]] == symbols[combo[1]] && symbols[combo[1]] == symbols[combo[2]]) {
+        return this.state.currentTurn
+      } else {
+        return false
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className="app-container">
+        {this.state.winner ? <h1>{`The winner is ${this.state.winner}`}</h1> : null}
+        <div className="board">
+        {this.state.board.map((cell, index) => {
+          return <div onClick={() => this.handleClick(index)} className="square">{cell}</div>;
+        })}
+        </div>
+      </div>
+    )
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default App;
